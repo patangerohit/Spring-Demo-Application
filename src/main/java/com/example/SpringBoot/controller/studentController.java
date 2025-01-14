45b89fc7c28ded9539dcadc9669aa925b57451ff
patangerohit <rohitrpatange2003@gmail.com>
2025-01-14 06:12:39 +0530

    controller update

--- src/main/java/com/example/SpringBoot/controller/studentController.java
+++ src/main/java/com/example/SpringBoot/controller/studentController.java
@@ -1,4 +1,5 @@
 package com.example.SpringBoot.controller;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.SpringBoot.dto.student;
+import com.example.SpringBoot.entity.studentEntity;
+import com.example.SpringBoot.service.mailService;
 import com.example.SpringBoot.service.studentService;
+import com.mysql.jdbc.log.Log;
+
 import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
@@ -17,11 +22,21 @@ public class studentController {
 @Autowired
 studentService service;
 
+@Autowired
+mailService mail;
+
 @PostMapping("/add")
 public void addStudent(@RequestBody student s) {
 	try {
 		service.add(s);
 		log.info("success");
+		try {
+		mail.sendMail(s.getEmail(), "Student Admission", "Success");
+		log.info("Mail sent");
+		}
+		catch(Exception e) {
+			log.info("Could not sent mail");
+		}
 	}
 	catch(Exception e) {
 		log.info("failed",e);
@@ -31,6 +46,11 @@ public void addStudent(@RequestBody student s) {
 @GetMapping("/find/{id}")
 public Optional<student> getStudent(@PathVariable long id) {
 	return Optional.ofNullable(service.convertToDTO(service.find(id).orElse(null)));
+}
+
+@GetMapping("/all")
+public List<studentEntity> getAll(){
+	return service.getAll();
 }
 
 }
